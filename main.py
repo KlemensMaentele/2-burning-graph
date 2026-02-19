68b75b97c942283cfce12876f9ad3385c9def9b0
joris <j.mantele@student.mastrichtuniversity.nl>
2026-02-19 15:00:49 +0100

    Added some pruning using burned state bitmap. Added leaf burning heuristic

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import random
 from line_profiler_pycharm import profile
+
 best_round = 9999999
 best_sequence = []
-
+checked_sequences = dict()
 
 def get_color_map(G):
     color_map = []
@@ -20,45 +21,9 @@ def draw_graph(G, pos):
     nx.draw(G, pos, node_color=color_map, with_labels=True)
     plt.show()
 
-@profile
-def alg(G, burned_nodes_set, unburned_nodes_set, unburned_nodes_list, nodes_dict, pos):
-    todo_nodes = list()
-    round = 0
-    while len(unburned_nodes_set) > 0:
-        round += 1
-        print(f"Round {round}")
-
-        todo_nodes = do_todo_nodes(G, todo_nodes, burned_nodes_set, unburned_nodes_set, unburned_nodes_list, nodes_dict)
-
-        if len(unburned_nodes_set) == 0:
-            # draw_graph(G, pos)
-            return  # done
-
-        print("Unburned nodes left :", unburned_nodes_set)
-
-        random_number_index = random.randint(0, len(unburned_nodes_list)-1)
-        node_to_burn = unburned_nodes_list[random_number_index]
-
-        print("Picked Burning node :", node_to_burn)
-
-        nodes_dict[node_to_burn] = 2
-
-        del unburned_nodes_list[random_number_index]
-
-        unburned_nodes_set.remove(node_to_burn)
-
-        burned_nodes_set.add(node_to_burn)
-
-        G.nodes[node_to_burn]["state"] = "burned"
-
-        todo_nodes.append(node_to_burn)
-
-       # draw_graph(G, pos)
-    print("All nodes burned!")
-    return
 
 @profile
-def do_todo_nodes(G, todo_nodes, burned_nodes_set, unburned_nodes_set, unburned_nodes_list, nodes_dict): # basically updates if the nodes are burned or not and updates status
+def do_todo_nodes(G, todo_nodes, burned_nodes_set, unburned_nodes_set, unburned_nodes_list, nodes_dict, burned_bitmap): # basically updates if the nodes are burned or not and updates status
     new_todo_nodes = list()
     while len(todo_nodes) > 0:
         current_node = todo_nodes.pop()
@@ -67,15 +32,16 @@ def do_todo_nodes(G, todo_nodes, burned_nodes_set, unburned_nodes_set, unburned_
                 nodes_dict[neighbor] += 1
                 if(nodes_dict[neighbor] >= 2):
                     #print("Burned :", neighbor)
+                    burned_bitmap |= (1 << neighbor)
                     burned_nodes_set.add(neighbor)
                     unburned_nodes_set.remove(neighbor)
                     unburned_nodes_list.remove(neighbor)       # this takes O(n) time so bad you can fix with other data structures later todo
-                    G.nodes[neighbor]["state"] = "burned"
+                    #G.nodes[neighbor]["state"] = "burned"
                     new_todo_nodes.append(neighbor)
-    return new_todo_nodes
+    return new_todo_nodes, burned_bitmap
 
 def main():
-    graph_size = 15
+    graph_size = 100
 
     burned_nodes_set = set()
 
@@ -85,7 +51,6 @@ def main():
 
     G = nx.erdos_renyi_graph(graph_size, 0.2, seed=123, directed=False)
 
-
     pos = nx.spring_layout(G, seed=432)
 
     nx.set_node_attributes(G, "unburned", "state")
@@ -94,20 +59,43 @@ def main():
     print(list(G.edges))
 
     draw_graph(G,pos)
-    rec_alg(G, burned_nodes_set, unburned_nodes_set, unburned_nodes_list, nodes_dict, pos,[], 0, [])
+    rec_alg(G, burned_nodes_set, unburned_nodes_set, unburned_nodes_list, nodes_dict, pos,[], 0, [], 0)
 
     nx.set_node_attributes(G, "unburned", "state")
 
     play_sequence(G, best_sequence,pos)
 
 
+@profile
+def rec_alg(G, burned_nodes_set, unburned_nodes_set, unburned_nodes_list, nodes_dict, pos, todo, round, sequence, burned_bitmap):
+    #heuristic to burn the leaf nodes first
+    if(round == 0):
+        for i in range(len(G.nodes)):
+            node = i
+            if G.degree[node] <= 1:
+                sequence.append(node)
+                round=+1
+                nodes_dict[node] = 2
+                unburned_nodes_set.remove(node)
+                unburned_nodes_list.remove(node)
+                burned_nodes_set.add(node)
+                burned_bitmap |= (1 << node)
+                todo.append(node)
+                todo, burned_bitmap = do_todo_nodes(G, todo, burned_nodes_set, unburned_nodes_set, unburned_nodes_list, nodes_dict, burned_bitmap)
 
-def rec_alg(G, burned_nodes_set, unburned_nodes_set, unburned_nodes_list, nodes_dict, pos, todo, round, sequence):
+
     global best_round
+    global checked_sequences
     round += 1
+
+    if burned_bitmap in checked_sequences and checked_sequences[burned_bitmap] <= round:
+        return
+    checked_sequences[burned_bitmap] = round
+
     if(round >= best_round):
         return
-    todo = do_todo_nodes(G, todo, burned_nodes_set, unburned_nodes_set, unburned_nodes_list, nodes_dict)
+
+    todo, burned_bitmap = do_todo_nodes(G, todo, burned_nodes_set, unburned_nodes_set, unburned_nodes_list, nodes_dict, burned_bitmap)
     if len(unburned_nodes_set) == 0:
         global best_sequence
         if round < best_round:
@@ -127,34 +115,33 @@ def rec_alg(G, burned_nodes_set, unburned_nodes_set, unburned_nodes_list, nodes_
             unburned_nodes_list_backup = unburned_nodes_list.copy()
             nodes_dict_backup = nodes_dict.copy()
             todo_backup = todo.copy()
-            G_backup = G.copy()
+            old_burned_bitmap = burned_bitmap
+            #G_backup = G.copy()
 
             # make changes
             nodes_dict[node_to_burn] = 2
             unburned_nodes_set.remove(node_to_burn)
             unburned_nodes_list.remove(node_to_burn)
             burned_nodes_set.add(node_to_burn)
-            G.nodes[node_to_burn]["state"] = "burned"
+           # G.nodes[node_to_burn]["state"] = "burned"
             todo.append(node_to_burn)
             sequence.append(node_to_burn)
+            burned_bitmap |= (1 << node_to_burn)
             print(sequence)
 
-            rec_alg(G, burned_nodes_set, unburned_nodes_set, unburned_nodes_list, nodes_dict, pos, todo, round, sequence)
+            rec_alg(G, burned_nodes_set, unburned_nodes_set, unburned_nodes_list, nodes_dict, pos, todo, round, sequence, burned_bitmap)
 
             # restore
             burned_nodes_set = burned_nodes_set_backup
             unburned_nodes_set = unburned_nodes_set_backup
             unburned_nodes_list = unburned_nodes_list_backup
             nodes_dict = nodes_dict_backup
-            G = G_backup
+          #  G = G_backup
             todo = todo_backup
             sequence.pop()
+            burned_bitmap = old_burned_bitmap
 
 # def greedy_alg(G, burned_nodes_set, unburned_nodes_set, unburned_nodes_list, nodes_dict, pos):
-
-
-
-
 
 
 def play_sequence(G, sequence, pos):
@@ -168,7 +155,7 @@ def play_sequence(G, sequence, pos):
         round += 1
         print(f"Round {round}")
 
-        todo = do_todo_nodes(G, todo, burned_nodes_set, unburned_nodes_set, unburned_nodes_list, nodes_dict)
+        todo, bitmap = do_todo_nodes(G, todo, burned_nodes_set, unburned_nodes_set, unburned_nodes_list, nodes_dict, 0)
 
         if len(unburned_nodes_set) == 0:
             print("All nodes burned!")
