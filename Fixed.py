9182d1ccc498b9a4b4114c8021917b0d3667678e
joris <j.mantele@student.mastrichtuniversity.nl>
2026-05-05 20:31:22 +0200

    upload tree changes

--- Fixed.py
+++ Fixed.py
@@ -3,51 +3,132 @@ import matplotlib.pyplot as plt
 
 best_round = 9999999
 best_sequence = []
-graph_x_dim = 5
-graph_y_dim = 5
-graph_size = graph_x_dim*graph_y_dim
+graph_x_dim = 10
+graph_y_dim = 10
+#graph_size = graph_x_dim*graph_y_dim
+graph_size = 11
+
 list_of_best_sequences = []
 
 
 def main():
+    tree_setup()
+    #petersen_setup()
 
+def petersen_setup():
+    global list_of_best_sequences
+    global best_round
+
+    todo, unburned_nodes_list, nodes_dict, unburned_nodes_set, burned_nodes_set = graph_setup()
+    G = nx.petersen_graph()
+    nx.set_node_attributes(G, "unburned", "state")
+    pos = nx.spring_layout(G)
+    draw_graph(G, pos)
+
+    rec_solve(G, burned_nodes_set, unburned_nodes_set, unburned_nodes_list, nodes_dict, todo, 1, [])
+    print("Number of best sequences:", len(list_of_best_sequences))
+    print("First best sequence:", list_of_best_sequences[1])
+    print("best sequences", list_of_best_sequences)
+
+
+def tree_setup():
+    global list_of_best_sequences
+    global best_round
+    matching_found = True
+    for r in range(1):
+        matching_found = False
+
+        todo, unburned_nodes_list, nodes_dict, unburned_nodes_set, burned_nodes_set = graph_setup()
+        G = nx.random_labeled_tree(graph_size)
+        nx.set_node_attributes(G, "unburned", "state")
+        pos = nx.spring_layout(G)
+        draw_graph(G, pos)
+        leaf_nodes = get_leaf_nodes(G)
+
+        rec_solve(G, burned_nodes_set, unburned_nodes_set, unburned_nodes_list, nodes_dict, todo, 1,[])
+
+
+       # print("Best round:", best_round)
+        #print("Best sequence:", best_sequence)
+        print("Number of best sequences:", len(list_of_best_sequences))
+        print("First best sequence:", list_of_best_sequences[1])
+        print("leaf nodes", leaf_nodes)
+        print("number of leafs:", len(leaf_nodes))
+        print("best sequences", list_of_best_sequences)
+
+        for sequence in list_of_best_sequences:
+            matching = True
+            for i in range(len(leaf_nodes)):
+                if(sequence[i] not in leaf_nodes):
+                    matching = False
+                    #print("false:", sequence[i])
+                    break
+            if(matching == True):
+                matching_found = True
+                print("all good!")
+                break
+
+
+        list_of_best_sequences = []
+        best_round = 999
+
+        # if(matching_found == False):
+        #     draw_graph(G, pos)
+
+
+
+    #play_sequence(G, list_of_best_sequences[0], pos, True)
+
+
+def cube_setup():
     todo, unburned_nodes_list, nodes_dict, unburned_nodes_set, burned_nodes_set = graph_setup()
 
     G = nx.grid_2d_graph(graph_x_dim, graph_y_dim)  # create a grid graph
     G = nx.convert_node_labels_to_integers(G)  # relabel nodes to
 
-  #  G = nx.erdos_renyi_graph(graph_size, 0.1, seed=432)
+    #  G = nx.erdos_renyi_graph(graph_size, 0.1, seed=432)
     nx.set_node_attributes(G, "unburned", "state")
 
     pos = nx.spring_layout(G)
 
-    pos = nx.bfs_layout(G,0)    # use this layout for grids
+    pos = nx.bfs_layout(G, 0)  # use this layout for grids
 
     draw_graph(G, pos)
 
-    rec_solve(G, burned_nodes_set, unburned_nodes_set, unburned_nodes_list, nodes_dict, [], 1,[])
+    # rec_solve(G, burned_nodes_set, unburned_nodes_set, unburned_nodes_list, nodes_dict, [], 1,[])
 
     ## For perimeter testing
-    #todo, unburned_nodes_list, unburned_nodes_set, nodes_dict, burned_nodes_set = burn_sequence(G, get_diagonal_sequence())
+    # todo, unburned_nodes_list, unburned_nodes_set, nodes_dict, burned_nodes_set = burn_sequence(G, get_diagonal_sequence())
 
-    #rec_solve(G, burned_nodes_set, unburned_nodes_set, unburned_nodes_list, nodes_dict, todo, len(get_diagonal_sequence())+1, get_diagonal_sequence(), True, get_perimiter())
-
+    # rec_solve(G, burned_nodes_set, unburned_nodes_set, unburned_nodes_list, nodes_dict, todo, len(get_diagonal_sequence())+1, get_diagonal_sequence(), True, get_perimiter())
 
     print("Best round:", best_round)
     print("Best sequence:", best_sequence)
     print("Number of best sequences:", len(list_of_best_sequences))
-    #print("Best sequences:", list_of_best_sequences)
+    # print("Best sequences:", list_of_best_sequences)
 
     diagonal_sequence = get_diagonal_sequence()
 
-    #11x11 sequences
-    r_19_11_11 = diagonal_sequence+ [76,115, 54, 113, 32, 110, 10]
+    # 11x11 sequences
+    r_19_11_11 = diagonal_sequence + [76, 115, 54, 113, 32, 110, 10]
 
-    addition_sequence = diagonal_sequence+ [76,115, 43, 112, 10, 110]
+    addition_sequence = diagonal_sequence + [76, 115, 43, 112, 10, 110]
+    seq = [65, 78, 52, 91, 39, 104, 26, 117, 13, 130, 0, 143]
 
-   # play_sequence(G,addition_sequence, pos, graph_size, True)
+    # play_sequence(G,addition_sequence, pos, graph_size, True)
 
-    play_sequence(G, list_of_best_sequences[1], pos, True)
+    # play_sequence(G, list_of_best_sequences[1], pos, True)
+    play_sequence(G, seq, pos, True)
+
+
+def get_leaf_nodes(G):
+    leaf_nodes = []
+    for g in G.nodes:
+        if(len(list(G.neighbors(g))) == 1):
+            leaf_nodes.append(g)
+
+    return leaf_nodes
+
 
 def get_perimiter():         # Only works for x and y the same
     perimiter = []            # todo change to set maybe if you really don't need list cannot be asked right now
@@ -136,7 +217,7 @@ def rec_solve(G, burned_nodes_set, unburned_nodes_set, unburned_nodes_list, node
             if(round == best_round):
                 list_of_best_sequences.append(sequence.copy())
             else:
-                list_of_best_sequences = [[]]
+                list_of_best_sequences = []
                 best_round = round
                 list_of_best_sequences.append(sequence.copy())
                 print(len(list_of_best_sequences))
@@ -173,7 +254,7 @@ def rec_solve(G, burned_nodes_set, unburned_nodes_set, unburned_nodes_list, node
                 if (round == best_round):
                     list_of_best_sequences.append(sequence.copy())
                 else:
-                    list_of_best_sequences = [[]]
+                    list_of_best_sequences = []
                     best_round = round
                     list_of_best_sequences.append(sequence.copy())
                     print(len(list_of_best_sequences))
@@ -204,7 +285,7 @@ def draw_graph(G, pos):
             color_map.append("gray")
 
     plt.figure(figsize=(8, 6))
-    nx.draw(G, pos, with_labels=True, node_color=color_map, node_size=500)
+    nx.draw(G, pos, with_labels=True, node_color=color_map, node_size=700)
     plt.show()
 
 
